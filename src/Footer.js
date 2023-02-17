af53dbcde76a26bbb3aed71eb671749281ca2712
Ganesh Bhise <ganeshbhise0205@gmail.com>
2023-02-17 17:31:12 +0530

    first commit

--- src/Footer.js
+++ src/Footer.js
@@ -2,6 +2,7 @@ const Footer = ({ length }) => {
   return (
       <footer>
           <p>{length} List {length === 1 ? "item" : "items"}</p>
+          <p>Ganesh Bhise|Copyright &copy;|2023</p>
       </footer>
   )
 }
